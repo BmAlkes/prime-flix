643d13d74030b76d7ad0c621f46467850c91b82a
bmalkes <bmalkes@gmail.com>
2023-04-26 12:29:51 +0300

    feat:add home page with load movies

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./styles.css";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function loadMovies() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "4aea5aea253fc563265bdd3d7cab46c1",
+          language: "en-US",
+          pages: [1, 2],
+        },
+      });
+      setMovies(response.data.results);
+    }
+    loadMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div className="container">
+      <div className="lista-filmes">
+        {movies.map((movie) => {
+          return (
+            <article key={movie.id}>
+              <strong>{movie.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <Link to={`/movies/${movie.id}`}>Acess Movie</Link>
+            </article>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
